01b11e3b3c6dc475a1711325dae99a6cf0ce92a8
Lloyd Briggs <lloyd.briggs@daimler.com>
2024-08-11 13:39:07 -0700

    project setup complete

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,29 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
+  dev: {
+    // It is necessary to configure assetPrefix, and in the production build, you need to configure output.assetPrefix
+    assetPrefix: 'http://localhost:3001',
+  },
+  tools: {
+    rspack: (config, { appendPlugins }) => {
+      // You need to set a unique value that is not equal to other applications
+      config.output!.uniqueName = 'remote_one';
+      appendPlugins([
+        new ModuleFederationPlugin({
+          name: 'remote_one',
+          exposes: {
+            './button': './src/Button.tsx',
+          },
+          shared: ['react', 'react-dom'],
+        }),
+      ]);
+    },
+  },
   plugins: [pluginReact()],
 });

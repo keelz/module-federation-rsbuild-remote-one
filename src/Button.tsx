5179872f54dfa0ded68d25bde3c383968b952984
Lloyd Briggs <lloyd.briggs@daimler.com>
2024-08-12 22:05:01 -0700

    stable

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,14 @@
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+const handleClick = () => console.warn('remote one, checking in');
 
 export default function ButtonUsoage() {
-  return <Button variant="contained">Remote One</Button>
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Button onClick={handleClick}>Remote One</Button>
+      </Grid>
+    </Grid>
+  );
 }
